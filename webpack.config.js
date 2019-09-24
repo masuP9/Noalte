7a2df0546dd8de6459bf53752c4b96f0f3764433
masuP9 <masuP9@gmail.com>
2019-09-25 00:18:44 +0900

    feat: build command for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,30 @@
+const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
-const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+module.exports = (env, argv) => {
+  const IS_PRODUCTION = argv.mode === 'production';
 
-module.exports = {
-  devtool: IS_PRODUCTION ? null : 'source-map',
-  entry: './src/scripts/main.tsx',
-  output: { filename: 'content_script.js' },
-  module: {
-    rules: [
-      {
-        test: /\.ts(x?)$/,
-        exclude: /node_modules/,
-        use: 'ts-loader',
-      },
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        loader: 'source-map-loader',
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-  },
-  plugins: [new CopyPlugin(['src/manifest.json', 'src/icons'])],
+  return {
+    devtool: 'source-map',
+    entry: './src/scripts/main.tsx',
+    output: { filename: 'content_script.js', path: path.resolve(__dirname, IS_PRODUCTION ? 'publish' : 'dist') },
+    module: {
+      rules: [
+        {
+          test: /\.ts(x?)$/,
+          exclude: /node_modules/,
+          use: 'ts-loader',
+        },
+        {
+          enforce: 'pre',
+          test: /\.js$/,
+          loader: 'source-map-loader',
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    plugins: [new CopyPlugin(['src/manifest.json', 'src/icons'])],
+  };
 };
